a184ce504158e733efacd15cdbd0ccde5e4ea670
yoohyunha <yoohyunha@naver.com>
2026-08-11 14:02:07 +0900

    dd
    dd

--- Workloads/Workloads/gallery-spring-boot/src/main/java/io/kickscar/cloud/workload/gallery/storage/ImageStorage.java
+++
@@ -1,9 +0,0 @@
-package io.kickscar.cloud.workload.gallery.storage;
-
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImageStorage {
-    String upload(MultipartFile file);
-    void delete(String url);
-}
